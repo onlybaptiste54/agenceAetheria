d1e01d2fd6741eb9116e1fbe637ad4f54eeac88d
OnlyBaptiste <157699823+OnlyBaptiste@users.noreply.github.com>
2025-10-15 17:04:56 +0200

    modification du sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   siteUrl: 'https://www.agenceaetheria.com',
   generateRobotsTxt: true,
+  // Ensure legal pages are present even if auto-discovery misses them
+  additionalPaths: async (config) => {
+    return [
+      await config.transform(config, '/privacy'),
+      await config.transform(config, '/utilisation'),
+    ];
+  },
 };
